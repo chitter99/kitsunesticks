65ac80cbc147a886c202e3281554ef3fcb398e1e
chitter99 <aaron.schmid@outlook.com>
2018-06-05 11:39:41 +0200

    Added config and started implementing App

---
+++ app/config.php
@@ -0,0 +1,16 @@
+<?php
+
+define("CONFIG", [
+    "db" => [],
+    "core" => [
+        "default" => [
+            "action" => "index",
+            "controller" => "home"
+        ],
+        "views" => INC_ROOT . "views/",
+        "services" => INC_ROOT . "service/",
+        "models" => INC_ROOT . "model/",
+        "controllers" => INC_ROOT . "controller/",
+    ]
+]);
+
--- app/core/app.php
+++ app/core/app.php
@@ -2,5 +2,75 @@
 
 class App
 {
+    function __construct()
+    {
+        $this->bootstrap();
+    }
 
+    function bootstrap()
+    {
+        $request = $this->checkRequest($this->parseURL());
+        $this->execRequest($request);
+    }
+
+    function checkRequest($request)
+    {
+        if(!$this->isController($request["controller"]) || !$this->isActionOnController($request["controller"], $request["action"])) {
+            $request = [
+                "controller" => "error",
+                "action" => "404"
+            ];
+        }
+        return $request;
+    }
+
+    function parseURL()
+    {
+        $controller = $_GET["controller"];
+        if(!isset($_GET["controller"])) {
+            $controller = CONFIG["core"]["default"]["controller"]; // Default Controller
+        }
+        $action = $_GET["action"];
+        if(!isset($_GET["action"])) {
+            $action = CONFIG["core"]["default"]["action"]; // Default Controller Action
+        }
+        return array(
+            "controller" => $controller,
+            "action" => $action
+        );
+    }
+
+    function execRequest($request)
+    {
+
+    }
+
+    public function getController($controller)
+    {
+        require(CONFIG["core"]["controllers"] . $controller . ".php");
+        $controller = new $controller;
+        $controller->onLoad($this);
+        return $controller;
+    }
+
+    public function isController($controller)
+    {
+        return file_exists(CONFIG["core"]["controllers"] . $controller . ".php");
+    }
+
+    public function isActionOnController($controller, $action)
+    {
+        return method_exists($controller, "_" . $action);
+    }
+
+    public function getService($service)
+    {
+        require(CONFIG["core"]["services"] . $service . ".php");
+        return new $service;
+    }
+
+    public function isService($service)
+    {
+        return file_exists(CONFIG["core"]["services"] . $service . ".php");
+    }
 }
